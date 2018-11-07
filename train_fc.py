d7507c76e60f6cdb1f4ece72bf683ae8e415a904
Yihe Dong <yihdong@microsoft.com>
2018-11-07 10:34:36 -0800

    fix minor bugs in train fc layer

--- train_fc.py
+++ train_fc.py
@@ -5,6 +5,8 @@ Train fully connected last layer, fixing weights of truncated net.
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import pdb
+import os
 
 import model
 
@@ -20,6 +22,7 @@ class FCL():
     def __init__(self, net, num_layers):
         
         self.n_out = 512
+        self.num_layers = num_layers
         if num_layers >= 21:
             self.n_out = 256 
         self.fc = nn.Linear(self.n_out, 10)
@@ -29,13 +32,13 @@ class FCL():
     Train fc and save
     Input: path to save
     '''
-    def train_fc(self, trainloader, testloader, epochs=200, path='pretrained'):
-
+    def train_fc(self, trainloader, testloader, epochs=50, path='pretrained'):
+        print('training for peeling off {} layers'.format(self.num_layers))
         optimizer = optim.SGD(self.fc.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4) #######
         criterion = nn.CrossEntropyLoss()
         best_acc = 0
         #every number of epochs to test
-        test_interval = 20
+        test_interval = 10
         test_total = len(testloader.dataset)
 
         if use_gpu:
@@ -49,7 +52,8 @@ class FCL():
                     input = input.to(device)
                     target = target.to(device)
                 with torch.no_grad():                    
-                    x = self.pretrained(input)
+                    x = self.pretrained(input).squeeze()
+                #print('x size {}'.format(x.size()))    
                 x = self.fc(x)
                 optimizer.zero_grad()
                 loss = criterion(x, target)
@@ -58,18 +62,20 @@ class FCL():
                 
             if (epoch + 1) % test_interval == 0:
                 correct = 0
-
+                #pdb.set_trace()
                 with torch.no_grad():
                     for i, (input, target) in enumerate(testloader):
                         input = input.to(device)
                         target = target.to(device)
-                        x = self.pretrained(input)
+                        x = self.pretrained(input).squeeze()
                         x = self.fc(x)
                         cls = x.max(dim=1)[1]
                         correct += cls.eq(target).sum()
-                acc = correct/test_total
-                if epoch > 100 and acc > best_acc:
-                    print('best acc for num_layers {}: {}'.format(self.num_layers, best_acc))
+                        
+                acc = int(correct)/test_total
+                #print('acc {} '.format(acc))
+                if epoch > 20 and acc > best_acc:
+                    print('best acc after peeling off num_layers {}: {}'.format(self.num_layers, acc))
                     save_path = os.path.join(path, 'fc'+str(self.num_layers) + '.t7')
                     torch.save(self.fc.state_dict(), save_path)
                     best_acc = acc
@@ -79,7 +85,8 @@ if __name__ == '__main__':
     #this model has all layers
     net = model.get_model()
     num_layers_l = [5, 8, 11, 15, 18, 21, 25, 28]
-    num_layers_l = [5]
+    num_layers_l = [11, 15, 18, 21, 25, 28]
+    #num_layers_l = [8]
     
     trainloader = model.get_loader(train=True)
     testloader = model.get_loader(train=False)
@@ -87,4 +94,4 @@ if __name__ == '__main__':
     for num_layers in num_layers_l:
         cur_model = model.peel_layers(net, num_layers)
         fcl = FCL(cur_model, num_layers)
-        fcl.train_fc(trainloader, testloader, epochs=200)
+        fcl.train_fc(trainloader, testloader, epochs=50)
